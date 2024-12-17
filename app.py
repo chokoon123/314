8351cf01e47f2f71e4491f136e8ba8e392cbab49
chokoon4686@gmail.com <chokoon4686@gmail.com>
2024-12-17 23:19:15 +0700

    final

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import ast
 import pandas as pd
 import re
 
-df = pd.read_csv("final_2.csv",usecols=["code","description","faculty","valid_pairs1_65","valid_pairs2","valid_pairs3"])
+df = pd.read_csv("final_pairs.csv",usecols=["code","description","faculty","valid_pairs1_65"])
 
 # Page Configuration
 
